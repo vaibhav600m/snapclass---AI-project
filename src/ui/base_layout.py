471a1cd22b8c50dc87c6ca58a0c06a9292402bac
Vaibhav Mathur <vaibhav600m@email.com>
2026-09-17 23:59:03 +0530

    Update base layout

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -43,15 +43,15 @@ def style_base_layout():
         @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
 
                 
-        #  /* Hide Top Bar of streamlit */
+         /* Hide Top Bar of streamlit */
                 
-        #     #MainMenu, footer, header {
-        #         visibility: hidden;
-        #     }
+            #MainMenu, footer, header {
+                visibility: hidden;
+            }
                 
-        #     .block-container {
-        #         padding-top:1.5rem !important;    
-        #     }
+            .block-container {
+                padding-top:1.5rem !important;    
+            }
 
             h1 {
                 font-family: 'Climate Crisis', sans-serif !important;
